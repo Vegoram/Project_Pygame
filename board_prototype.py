66603936eed617658af30f508c08a61058343e1a
Арсений <94627349+Arz2212@users.noreply.github.com>
2022-01-06 20:58:34 +0300

    Update board_prototype.py

--- board_prototype.py
+++ board_prototype.py
@@ -9,7 +9,7 @@ clock = pygame.time.Clock()
 pygame.display.set_caption('Стрельботня')
 all_sprites = pygame.sprite.Group()
 field = BattleField('test_map', 90, (50, 50), screen)
-field.add_unit('Cannon', 0, 0)
+field.add_unit('Cannon', 3, 0)
 field.add_unit('TankSmall', 7, 2, True)
 field.add_unit('Artillery', 3, 3)
 field.add_unit('Cannon', 9, 9, True)
@@ -20,15 +20,27 @@ field.add_unit('Cannon', 0, 1, True)
 field.render()
 pygame.display.flip()
 running = True
+move = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             field.tap_dispatcher(event.pos, event.button)
+            if event.button == 2:
+                if move:
+                    field.moving(hed1, event.pos)
+                    move = False
+                else:
+                    move = True
+                    hed1 = event.pos
+            else:
+                move = False
+            print(event.button)
     field.update()
     screen.fill((0, 0, 0))
     field.render()
     clock.tick(FPS)
     pygame.display.flip()
 terminate()
+
